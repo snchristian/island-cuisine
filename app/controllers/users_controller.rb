3e2129c943fb35c7a63c09a54f0f00c15336b5ea
Shantel <snchristian8@gmail.com>
2022-07-01 12:19:36 -0400

    add user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    
+    def index
+        @user = User.all
+
+        render json: @dogs
+    end
+
+    def show
+        render json: @user
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unproccesable_entity
+        end
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username,:password,:email)
+    end
 end

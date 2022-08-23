9023b926a5e627511acf2b263ad68ff0b8d31103
Shantel <snchristian8@gmail.com>
2022-08-22 21:50:20 -0400

    Add styling and functionality to homepage

---
+++ client/src/components/Static/HomeStyle.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Recipestyle = styled.div`
+width:100vw;
+height:100vh;
+display:flex;
+align-items:center;
+justify-content:center;
+background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(https://www.trainingspaces.ca/wp-content/uploads/2019/03/maldives-FOODIE1215.jpg);
+text-align:center;
+background-size:cover;
+}
+
+
+ h1{
+    margin-bottom:30px;
+    font-size:80px;
+}
+
+p{
+    font-size:25px;
+    line-height:24px;
+    margin-bottom: 40px;
+}
+
+button{
+    width:220px;
+    padding:12px 0;
+    color:#fff;
+    font-size:20px;
+    border-radius:5px;
+    outline:none;
+    border:2px solid #fff;
+    background:transparent;
+    cursor:pointer;
+}
+
+`
+
+ export const Text = styled.div`
+ color:#fff;
+ width:60%;
+
+`

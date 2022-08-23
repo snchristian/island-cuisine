3bad8322221f525fc22c6da99153c2a760ee482d
Shantel <snchristian8@gmail.com>
2022-08-22 22:04:09 -0400

    add favorite component and controller

--- app/controllers/favorite_recipes_controller.rb
+++ app/controllers/favorite_recipes_controller.rb
@@ -1,3 +1,25 @@
 class FavoriteRecipesController < ApplicationController
+    
+  def create
+        @favortie_recipes =  current_user.FavoriteRecipe.new(favorites_params)
 
+        if @favortie_recipes.save
+            render json: @favortie_recipes, status: :created
+        else
+            render json: @favortie_recipes.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+    end
+
+    private
+
+    # def set_recipe
+    #     @recipe = Recipe.find(params[:id])
+    # end
+
+    def favorites_params
+        params.require(:favortie_recipes).permit(:recipe_id)
+    end
 end
